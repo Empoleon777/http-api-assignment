0f6d2cb0b530eb04480a6bfe958e742b50a4b316
William Fox <bcf7023@rit.edu>
2024-02-06 17:04:03 -0500

    What I have so far.

---
+++ src/xmlResponses.js
@@ -0,0 +1,95 @@
+const respondXML = (request, response, status, object) => {
+    response.writeHead(status, { 'Content-Type': 'text/xml' });
+    response.write(object);
+    response.end();
+};
+
+const success = (request, response) => {
+    let responseXML = `<response>`;
+    responseXML += `<message>This is a successful response.</message>`;
+    responseXML += `</response>`;
+
+    respondXML(request, response, 200, responseXML);
+};
+
+const badRequest = (request, response, params) => {
+    let responseXML = '<response>';
+
+    if (!params.valid || params.valid !== 'true') {
+        responseXML += `<message>Missing valid query parameter set to true.</message>`;
+        responseXML += `<id>Bad Request</id>`;
+        responseXML += `</response>`;
+
+        return respondXML(request, response, 400, responseXML);
+    }
+    else {
+        responseXML += `<message>This request has the required parameters.</message>`;
+        responseXML += `</response>`;
+
+        return respondXML(request, response, 200, responseXML);
+    }
+};
+
+const unauthorized = (request, response) => {
+    let responseXML = `<response>`;
+
+    if (!params.valid || params.valid !== 'true') {
+        responseXML += `<message>Missing loggedIn query parameter set to yes.</message>`;
+        responseXML += `<id>Unauthorized</id>`;
+        responseXML += `</response>`;
+
+        return respondXML(request, response, 401, responseXML);
+    }
+    else {        
+        responseXML += `<message>You have successfully viewed the content.</message>`;
+        responseXML += `</response>`;
+        return respondXML(request, response, 200, responseXML);
+    }
+}
+
+const forbidden = (request, response) => {
+    let responseXML = `<response>`;
+    responseXML += `<message>You do not have access to this content.</message>`;
+    responseXML += `<id>Forbidden</id>`;
+    responseXML += `</response>`;
+
+    return respondXML(request, response, 403, responseXML);
+}
+
+const internal = (request, response) => {
+    let responseXML = `<response>`;
+    responseXML += `<message>Internal Server Error. Something went wrong.</message>`;
+    responseXML += `<id>Internal</id>`;
+    responseXML += `</response>`;
+
+    return respondXML(request, response, 500, responseXML);
+}
+
+const notImplemented = (request, response) => {
+    let responseXML = `<response>`;
+    responseXML += `<message>A get request for this page has not been implemented yet. Check again later for updated content.</message>`;
+    responseXML += `<id>Not Implemented</id>`;
+    responseXML += `</response>`;
+
+    return respondXML(request, response, 501, responseXML);
+}
+
+const notFound = (request, response) => {
+    let responseXML = `<response>`;
+    responseXML += `<message>The page you are looking for was not found.</message>`;
+    responseXML += `<id>Not Found</id>`;
+    responseXML += `</response>`;
+
+    // return our json with a 404 not found error code
+    respondXML(request, response, 404, responseXML);
+};
+
+module.exports = {
+    success,
+    badRequest,
+    unauthorized,
+    forbidden,
+    internal,
+    notImplemented,
+    notFound
+};  
